e44d64a7c134843689fb2e596257ecb1c2c6ea89
ahmd-fariz <ahmadfariz4@gmail.com>
2024-03-10 21:36:11 +0700

    first commit

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\User;
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class UserController extends BaseController
+{
+    public function index()
+    {
+        $usermodel = new User;
+        $user = $usermodel->findAll();
+        dd($user);
+    }
+}
